769eb183ec17b69d74b7ba41d8d106bdaa456b00
evertondsvieira <vieiraeverton33@hotmail.com>
2023-04-14 16:57:37 -0300

    feat: added cart route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import Cart from "../components/Cart";
 
 const Router = () => {
 
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="" />
+            <Route path="/cart" element={<Cart />} />
         </Routes>
     )
 }
